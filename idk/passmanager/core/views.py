4020ae2337b1bd815724bfadf5b9d01ab877a951
Jaijan <jaijan.23aiml@cambridge.edu.in>
2025-05-12 20:32:20 +0530

    Add files via upload

--- idk/passmanager/core/views.py
+++ idk/passmanager/core/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from .models import Credential, UserProfile
+from .forms import CredentialForm
 
 def home(request):
-    return render(request, 'core/home.html')
+    # For demo, use the first user (in production, use the logged-in user)
+    user = UserProfile.objects.first()
+    if not user:
+        user = UserProfile.objects.create(name='Default', pin='0000')
+
+    if request.method == 'POST':
+        website = request.POST.get('website')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if website and username and password:
+            cred = Credential(user=user, website=website, username=username)
+            cred.set_password(password)
+            cred.save()
+    
+    credentials = Credential.objects.filter(user=user)
+    # Prepare credentials for JS (decrypted)
+    cred_list = [
+        {
+            'website': c.website,
+            'username': c.username,
+            'password': c.get_password(),
+        } for c in credentials
+    ]
+    return render(request, 'core/home.html', {
+        'credentials': cred_list,
+    })
